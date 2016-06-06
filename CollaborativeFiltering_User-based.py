f68f672c64625cbb444bea16475a1a64ab3079e6
Vaibhavi15 <paivaibhavi15@gmail.com>
2016-06-06 22:55:08 +0530

    Code according to pep8 standards

--- CollaborativeFiltering_User-based.py
+++ CollaborativeFiltering_User-based.py
@@ -1,98 +1,101 @@
-# # Collaborative Filtering #user Based collaborative Filtering
+# Collaborative Filtering #User Based collaborative Filtering
 
-#Import libraries
+# Import libraries
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import cosine
 
 
-class functions:
+class Functions: 
 
-    def createDf(self, dindex, dcolumns):
+    def create_df(self, dindex, dcolumns):
         return pd.DataFrame(index=dindex, columns=dcolumns)
 
-    def findSimilarity(self, dataToBeFilled, dataUsed):                   # Similarity Measure using cosine distances
-        for i in range(0,len(dataToBeFilled.columns)):
-            for j in range(0,len(dataToBeFilled.columns)):                #Loop through the columns for each column
-                dataToBeFilled.ix[i,j] = cosine(dataUsed.ix[:,i],dataUsed.ix[:,j])
+    def find_similarity(self, data_to_be_filled, data_used):                  # Similarity Measure using cosine distances
+        for i in range(0, len(data_to_be_filled.columns)):
+            for j in range(0, len(data_to_be_filled.columns)):                # Loop through the columns for each column
+                data_to_be_filled.ix[i, j] = cosine(data_used.ix[:, i],data_used.ix[:, j])
 
-        return dataToBeFilled
+        return data_to_be_filled
 
     def swap(a,b):
         return b,a
+    
 
-class run:
+class Run:
 
-    def __init__(self,file):
+    def __init__(self, file):
 
         self.data=pd.read_csv(file)
-        self.data["Quantity"]=1          #Assume that for all items only one quantity was bought
-        self.dataWide=self.data.pivot("Person", "item", "Quantity")           #Converting data from long to wide format
-        self.dataWide.fillna(0, inplace=True)            #Replace NA with 0
+        self.data["Quantity"] = 1                                               # Assume that for all items only one quantity was bought
+        self.dataWide = self.data.pivot("Person", "item", "Quantity")           # Converting data from long to wide format
+        self.dataWide.fillna(0, inplace=True)                                   # Replace NA with 0
         self.data_purchases = self.dataWide.copy()
-        self.data_purchases = self.data_purchases.reset_index()             #To make the 'Person' field just another column and not an index
-        self.data_purchases = self.data_purchases.drop("Person", axis=1)
+        self.data_purchases = self.data_purchases.reset_index()
+        self.data_purchases = self.data_purchases.drop("Person", axis=1)  # To make the 'Person' field just another column and not an index
         self.data_itemvsuser = self.data_purchases.copy()
 
     def main(self):
-        user1=filter()
-        user1.getRecommendation(10, 10)
+        user1 = Filter()
+        user1.get_recommendation(10, 10)
 
 
-class user(run):
+class User(Run):
 
     def __init__(self):
 
-        self.fun=functions()
-        self.user_sim_smaller= self.userSimilarity()
-        self.user_neighbours= self.userNeighbours()
-        self.user_purchases=  self.userPurchases()
+        self.fun = Functions()
+        self.user_sim_smaller = self.user_similarity()
+        self.user_neighbours = self.user_neighbours()
+        self.user_purchases =  self.user_purchases()
 
+    def user_similarity(self):
 
-    def userSimilarity(self):
-
-        self.user_sim = self.fun.createDf(self.data_itemvsuser.index, self.data_itemvsuser.index)
-        self.user_sim_smaller=self.user_sim.iloc[:100,:100]
-        self.user_sim_smaller= self.fun.findSimilarity(self.user_sim_smaller, self.data_purchases)
+        self.user_sim = self.fun.create_df(self.data_itemvsuser.index, self.data_itemvsuser.index)
+        self.user_sim_smaller = self.user_sim.iloc[:100, :100]
+        self.user_sim_smaller = self.fun.find_similarity(self.user_sim_smaller, self.data_purchases)
 
         return self.user_sim_smaller
 
+    def user_neighbours(self):       #Find neighbours to users based on similarity
 
-    def userNeighbours(self):       #Find neighbours to users based on similarity
-
-        self.user_neighbours = self.fun.createDf(self.user_sim_smaller.columns,range(1,11))
-        for i in range(0,len(self.user_sim_smaller.columns)):       # Loop through our similarity dataframe and fill in neighbouring item names
-            self.user_neighbours.ix[i, :] = np.array(self.user_sim_smaller.ix[i,:].sort_values(ascending=True)[0:10].index)
-            if self.user_neighbours.iloc[i,0] != i:
-                c=self.user_neighbours.iloc[i,:][self.user_neighbours.iloc[i,:] == i].index.tolist()
-                self.user_neighbours.iloc[i,0],self.user_neighbours.iloc[i,c[0]-1]=self.fun.swap(self.user_neighbours.iloc[i,0], self.user_neighbours.iloc[i,c[0]-1])
+        self.user_neighbours = self.fun.create_df(self.user_sim_smaller.columns,range(1, 11))
+        for i in range(0, len(self.user_sim_smaller.columns)):       # Loop through our similarity dataframe and fill in neighbouring item names
+            self.user_neighbours.ix[i, :] = np.array(self.user_sim_smaller.ix[i, :]
+                                                     .sort_values(ascending = True)[0:10].index)
+            if self.user_neighbours.iloc[i ,0] != i:
+                c=self.user_neighbours.iloc[i, :][self.user_neighbours.iloc[i, :] == i].index.tolist()
+                self.user_neighbours.iloc[i, 0],self.user_neighbours.iloc[i, c[0] - 1]\
+                    = self.fun.swap(self.user_neighbours.iloc[i, 0], self.user_neighbours.iloc[i, c[0] - 1])
         return self.user_neighbours
 
-    def userPurchases(self):      #Find all items purchased by a user
+    def user_purchases(self):      # Find all items purchased by a User
 
-        self.user_purchases=self.fun.createDf(self.user_sim_smaller.columns,range(max(self.data.Person.value_counts())))
+        self.user_purchases = self.fun.create_df(self.user_sim_smaller.columns,range(max(self.data.Person.value_counts())))
         for i in range(len(self.user_sim_smaller.columns)):
-           self.user_purchases.iloc[i,:]=pd.Series(self.data_purchases.iloc[i,:][self.data_purchases.iloc[i,:]==1].index.tolist())
-        self.user_purchases=self.user_purchases.fillna(0)
+           self.user_purchases.iloc[i, :] = pd.Series(self.data_purchases.iloc[i, :][self.data_purchases.iloc[i, :] == 1]
+                                                      .index.tolist())
+           self.user_purchases = self.user_purchases.fillna(0)
         return self.user_purchases
 
 
-class filter(user, run):
+class Filter(User, Run):
 
     def __init__(self):
-        run.__init__(self, "/home/vaibhavi/machine-learning/cf_mba/data/groceries.csv")
-        user.__init__(self)
+        Run.__init__(self, "/home/vaibhavi/machine-learning/cf_mba/data/groceries.csv")
+        User.__init__(self)
 
-    def getRecommendation(self, number, usernumber):            #getting 'number' number of recommendations
-        self.recommend=self.fun.createDf(self.user_sim_smaller.columns, range(1,11))
+    def get_recommendation(self, number, user_number):            # Getting 'number' number of recommendations
+        self.recommend = self.fun.create_df(self.user_sim_smaller.columns, range(1, 11))
         for j in range(len(self.user_sim_smaller.columns)):
-            self.itemslist=[]
-            for i in range(2,11):
-                self.items=[self.user_purchases.ix[self.user_neighbours.ix[j,i]][k] for k in range(10) if self.user_purchases.ix[self.user_neighbours.ix[j,i]][k]!=0]
-                self.itemslist=list(set(self.itemslist+self.items))
-            self.recommend.iloc[j,:]=pd.Series(self.itemslist[0:number+1])
-        print(self.recommend.iloc[usernumber,:])
+            self.itemslist = []
+            for i in range(2, 11):
+                self.items = [self.user_purchases.ix[self.user_neighbours.ix[j, i]][k] for k in range(10)
+                            if self.user_purchases.ix[self.user_neighbours.ix[j, i]][k] != 0]
+                self.itemslist = list(set(self.itemslist + self.items))
+            self.recommend.iloc[j, :] = pd.Series(self.itemslist[0:number + 1])
+        print(self.recommend.iloc[user_number, :])
 
 
 if __name__ == '__main__':
-    run("/home/vaibhavi/machine-learning/cf_mba/data/groceries.csv").main()
+    Run("/home/vaibhavi/machine-learning/cf_mba/data/groceries.csv").main()
