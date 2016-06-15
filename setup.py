bd2d763b68cc163aaa29fc1711a9f8f84cdfd112
Vaibhavi15 <paivaibhavi15@gmail.com>
2016-06-15 12:50:27 +0530

    Created first version of the package!

---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name='CollabFilter', version='1.0', description='Package for collaborative filtering', author='Rohit Sharma, Vaibhavi Pai',
+      py_modules=['collaborative-filtering'],)
